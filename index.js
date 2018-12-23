66042ee4c434400d01e3b0659fae5bf9f3024f8e
Dian Hanifudin Subhi <dhanifudin@gmail.com>
2018-12-23 20:26:00 +0700

    add uptime url into environment

--- index.js
+++ index.js
@@ -4,12 +4,13 @@ const Telegraf = require('telegraf')
 const Telegram = require('telegraf/telegram')
 const token = process.env.BOT_TOKEN
 const groupid = process.env.GROUP_ID
+const uptimeUrl = process.env.UPTIME_URL
 const telegram = new Telegram(token)
 const bot = new Telegraf({ telegram })
 const CronJob = require('cron').CronJob
 
 const job = new CronJob('0 */1 * * * *', () => {
-  axios.head('https://duniagames.co.id')
+  axios.head(uptimeUrl)
     .then(response => {
       const status = response.status;
       if (response.status != 200) {
