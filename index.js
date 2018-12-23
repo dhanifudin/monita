12472fc204693bce04fa3f8556a89b2026b12e82
Dian Hanifudin Subhi <dhanifudin@gmail.com>
2018-12-23 21:11:08 +0700

    reorganize as module and reduce notifications based on status

--- index.js
+++ index.js
@@ -1,27 +1,12 @@
+'use strict'
+
 require('dotenv').config()
-const axios = require('axios');
 const Telegraf = require('telegraf')
 const Telegram = require('telegraf/telegram')
 const token = process.env.BOT_TOKEN
-const groupid = process.env.GROUP_ID
-const uptimeUrl = process.env.UPTIME_URL
 const telegram = new Telegram(token)
 const bot = new Telegraf({ telegram })
-const CronJob = require('cron').CronJob
-
-const job = new CronJob('0 */1 * * * *', () => {
-  axios.head(uptimeUrl)
-    .then(response => {
-      const status = response.status;
-      if (response.status != 200) {
-        const message = `Server sedang bermasalah, status: ${status}`
-        telegram.sendMessage(groupid, message);
-      }
-    })
-    .catch(err => console.log(err))
-})
-
-job.start()
+const uptime = require('./uptime')(telegram)
 
 bot.start((ctx) => ctx.reply('Welcome!'))
 bot.startPolling()
