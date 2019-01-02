fe7f1ab099ec8f014888779dcf329b5701a5dc00
Dian Hanifudin Subhi <dhanifudin@gmail.com>
2019-01-02 19:50:36 +0700

    add artisan cache:clear command

--- index.js
+++ index.js
@@ -5,14 +5,16 @@ require('dotenv').config();
 const token = process.env.BOT_TOKEN;
 const enableUptime = process.env.ENABLE_UPTIME;
 const enableDf = process.env.ENABLE_DF;
+const enableArtisan = process.env.ENABLE_ARTISAN;
 
 const Telegraf = require('telegraf');
 const Telegram = require('telegraf/telegram');
 const telegram = new Telegram(token);
 const bot = new Telegraf(token, { telegram });
 
-(enableUptime) && require('./lib/uptime')(telegram);
-(enableDf) && require('./lib/df')(bot, telegram);
+(enableUptime == 'true') && require('./lib/uptime')(telegram);
+(enableDf == 'true') && require('./lib/df')(bot, telegram);
+(enableArtisan == 'true') && require('./lib/artisan')(bot);
 
 bot.start((ctx) => ctx.reply('Welcome!'));
 bot.help((ctx) => ctx.reply('Help'));
